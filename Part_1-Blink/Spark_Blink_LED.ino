7cd2d9e8fd7fbe22483d552f5faacfd1422a795a
JustNasci <jusnasci@bu.edu>
2025-04-03 00:52:10 -0400

    Updated "Blink" to be easier to read

--- Part_1-Blink/Spark_Blink_LED.ino
+++ Part_1-Blink/Spark_Blink_LED.ino
@@ -1,15 +1,23 @@
 // C++ code
 //
+
+// Defines D1 as 5, as per the Linux # Specified by the chip
+#define D1 5
+
 void setup()
 {
-  pinMode(5, OUTPUT);
+  // Defines D1 as an output, which makes sense as you are outputting power to control the LED
+  pinMode(D1, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(5, HIGH);
-  delay(2000); // Wait for 1000 millisecond(s)
-  digitalWrite(5, LOW);
+  // Drives D1 to output 5V, the "high" value
+  digitalWrite(D1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  // Drives D1 to output 0V, the "low" value
+  digitalWrite(D1, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
 
